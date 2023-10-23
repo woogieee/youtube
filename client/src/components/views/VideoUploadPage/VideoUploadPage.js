bff8a802a84213462c32f41ef9f2b43f58b69f2d
jay <p0425@nate.com>
2023-10-23 17:42:18 +0900

    Revert "1. 파일업로드 템플릿 Drop-zone 적용"
    This reverts commit b68ec04c3906ad7634fc767c460d86583fb59d4a.

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,192 +1,51 @@
-import React, { useState } from 'react'
-import { Typography, Button, Form, message, Input, Icon } from 'antd';
-import Dropzone from 'react-dropzone';
-import Axios from 'axios';
-import { useSelector } from 'react-redux';
+import React from 'react'
+import { Typography, Button, Form, message, Input, Icon } from 'antd'
 
 const { TextArea } = Input;
 const { Title } = Typography;
 
-
-const PrivateOptions = [
-    {value: 0, label: "Private"},
-    {value: 1, label: "Public"}
-]
-
-const CategoryOptions = [
-    {value: 0, label: "Film & Animation"},
-    {value: 1, label: "Autos & Vehicles"},
-    {value: 2, label: "Music"},
-    {value: 3, label: "Pets & Animals"}
-]
-
-function VideoUploadPage(props) {
-    const user = useSelector(state => state.user)
-    //user에 모든 정보를 가져옴
-    const [VideoTitle, setVideoTile] = useState("")
-    const [Description,setDescription] = useState("")
-    const [Private, setPrivate] = useState(0)
-    const [Category, setCategory] = useState("Film & Animation")
-    const [FilePath, setFilePath] = useState("")
-    const [Duration, setDuration] = useState("")
-    const [ThumbnailPath, setThumbnailPath] = useState("")
-
-    
-    //input, TextArea 텍스트 입력
-    const onTitleChange = (e) => {
-        setVideoTile(e.currentTarget.value)
-    }
-
-    const onDescriptionChange = (e) => {
-        setDescription(e.currentTarget.value)
-    }
-    
-    //Private, Category 변경
-    const onPrivateChange = (e) => {
-        setPrivate(e.currentTarget.value)
-    }
-
-    const onCategoryChange = (e) => {
-        setCategory(e.currentTarget.value)
-    }
-
-    //파일 업로드
-    const onDrop = (files) => {
-        let formData = new FormData;
-        const config = {
-            header: {'content-type': 'multipart/form-data'}
-        }
-        formData.append("file", files[0])
-
-        console.log(files)
-
-        Axios.post('/api/video/uploadfiles', formData, config)
-        .then(response => {
-            if(response.data.success) {
-                console.log(response.data)
-                
-                let variable = {
-                    url: response.data.url,
-                    fileName: response.data.fileName
-                }
-
-                setFilePath(response.data.url)
-
-                Axios.post('/api/video/thumbnail', variable)
-                .then(response => {
-                    if(response.data.success) {
-                        console.log(response.data)
-                        setDuration(response.data.fileDuration)
-                        //console.log(Duration)
-                        setThumbnailPath(response.data.url)
-                        //console.log(ThumbnailPath)
-
-                    } else {
-                        alert('썸네일 생성에 실패 했습니다.')
-                    }
-                })
-            } else {
-                alert('비디오 업로드를 실패했습니다.')
-            }
-        })
-    }
-
-    const onSubmit = (e) => {
-        e.preventDefault();
-
-        const variables = {
-            writer: user.userData._id,
-            title: VideoTitle,
-            description: Description,
-            privacy: Private,
-            filePath: FilePath,
-            category: Category,
-            duration: Duration,
-            thumbnail: ThumbnailPath,
-        }
-
-        Axios.post('/api/video/uploadVideo', variables)
-        .then(response => {
-            if(response.data.success) {
-                message.success('업로드에 성공 했습니다.')
-
-                setTimeout(() => {
-                    props.history.push('/')
-                }, 3000);
-                
-            } else {
-                alert('비디오 업로드에 실패 했습니다.')
-            }
-        })
-    }
-
-
-
+function VideoUploadPage() {
   return (
     <div style={{ maxWidth:'700px', margin:'2rem auto' }}>
         <div style={{ textAlign:'center', marginBottom:'2rem' }}>
             <Title level={2}>Upload Video</Title>
         </div>
         
-        <Form onSubmit={onSubmit}>
+        <Form onSubmit>
             <div style={{ display:'flex', justifyContent:'space-between' }}>
                 {/* Drop Zone */}
 
-                <Dropzone
-                onDrop={onDrop}
-                multiple={false}        //한번에 멀티로 업로드 가능한지
-                maxSize={100000000}>
-                    {({ getRootProps, getInputProps}) => (
-                        <div style={{ width: '300px', height: '240px', border: '1px solid lightgray', display: 'flex', 
-                        alignItems: 'center', justifyContent: 'center'}} {...getRootProps()}>
-                            <input {...getInputProps()} />
-                            <Icon type="plus" style={{ fontSize: '3rem'}} />
-                        </div>
-                    )}
-                </Dropzone>
-
                 {/* Thumbnail */}
-                {/* 썸네일이 있을경우만(&&) 표시 */}
-                {ThumbnailPath &&
-                    <div>
-                        <img src={`http://localhost:5000/${ThumbnailPath}`} alt="thumbnail" />
-                    </div>
-                
-                }
+                <div>
+                    <img src alt />
+                </div>
             </div>
             <br />
             <br />
             <label>Title</label>
             <Input 
-                onChange={onTitleChange}
-                value={VideoTitle}
+                onChange
+                value
             />
             <br />
             <br />
             <label>Description</label>
             <TextArea 
-                onChange={onDescriptionChange}
-                value={Description}
+                onChange
+                value
             />
             <br />
             <br />
 
-            <select onChange={onPrivateChange}>
-                {PrivateOptions.map((item, index) => (
-                    <option key={index} value={item.value}>{item.label}</option>
-                ))}
-                
+            <select onChange>
+                <option key value></option>
             </select>
-            <br />
-            <br />
-            <select onChange={onCategoryChange}>
-                {CategoryOptions.map((item, index) => (
-                    <option key={index} value={item.value}>{item.label}</option>
-                ))}
+
+            <select onChange>
+                <option key value></option>
             </select>
-            <br />
-            <br />
-            <Button type="primary" size="large" onClick={onSubmit}>
+
+            <Button type="primary" size="large" onClick>
                 Submit
             </Button>
         </Form>
